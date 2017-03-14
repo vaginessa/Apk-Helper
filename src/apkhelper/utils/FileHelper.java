24e5adba13733f495fbc23d7391094f62e6e2463
oscar0812 <oscar0812torres@gmail.com>
2017-03-13 19:27:01 -0500

    This class is non static now

--- src/apkhelper/utils/FileHelper.java
+++ src/apkhelper/utils/FileHelper.java
@@ -12,20 +12,20 @@ import java.io.File;
  * @author Oscar (AKA Bittle)
  */
 public class FileHelper {
-    public static boolean fileExists(String file){
+    public boolean fileExists(String file){
         return fileExists(new File(file));
     }
     
-    public static boolean fileExists(File file){
+    public boolean fileExists(File file){
         return file.exists();
     }
     
     // ======
     
-    public static boolean isDirectory(String file){
+    public boolean isDirectory(String file){
         return isDirectory(new File(file));
     }
-    public static boolean isDirectory(File file){
+    public boolean isDirectory(File file){
         if(file.exists()){
             return file.isDirectory();
         }
@@ -35,17 +35,55 @@ public class FileHelper {
     }
     
     
-    public static boolean fileIsApk(String file){
-        return fileIsApk(new File(file));
+    public boolean isAPK(String file){
+        return isAPK(new File(file));
     }
-    public static boolean fileIsApk(File file){
+    public boolean isAPK(File file){
         if(file.exists()){
-            return file.isFile();
+            return file.getAbsolutePath().endsWith(".apk");
         }else{
             return false;
         }
     }
-    public static File stringToFile(String s){
+    public File stringToFile(String s){
         return new File(s);
+    }
+    
+    public File getFileFromSrc(String fileName){
+        return getFileFromSrc("", fileName);
+    }
+    
+    public File getFileFromSrc(String path, String fileName){
+        if(!path.equals("")){
+            path+="/";
+        }
+        File file = new File("src/"+path+fileName);
+        if(file.exists()){
+            return file;
+        }
+        else{
+            System.out.println(file.getAbsolutePath());
+            System.out.println("File "+fileName+" does not exist");
+            return null;
+        }   
+    }
+    
+    public boolean isDecompiledApkDir(String dir){
+        return isDecompiledApkDir(new File(dir));
+    }
+    
+    public boolean isDecompiledApkDir(File file){
+        if(file.isDirectory()){
+            File manifest = new File(file.getAbsolutePath()+
+                    "/AndroidManifest.xml");
+            File res = new File(file.getAbsolutePath()+"/res/");
+            
+            return (manifest.isFile() && manifest.exists() 
+                    && res.isDirectory() && res.exists());
+            
+        }
+        else{
+            return false;
+        }
     }
 }
