857722e7e8d5da1530ddb4236b19cb9e13df6204
oscar0812 <oscar0812torres@gmail.com>
2017-03-13 19:39:25 -0500

    New about menu

---
+++ src/apkhelper/AboutFXMLController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apkhelper;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+
+/**
+ * FXML Controller class
+ *
+ * @author Oscar (AKA Bittle)
+ */
+public class AboutFXMLController implements Initializable {
+
+
+    
+    @FXML
+    private Text node;
+
+        /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        
+    }
+    
+    
+}
